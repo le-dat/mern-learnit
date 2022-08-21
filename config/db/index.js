c57e1a67eb97462ebd26dda464f82b6cd05e0a38
le-dat <lqdat87@gmail.com>
2022-08-21 23:44:41 +0700

    Fix mongo url to access anywhere 2

--- config/db/index.js
+++ config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports.connect = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mern-learnit.x2bqdo5.mongodb.net/?retryWrites=true&w=majority/${process.env.MONGODB}`
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mern-learnit.x2bqdo5.mongodb.net/?retryWrites=true&w=majority`
     );
     console.log("Connect successfully");
   } catch (err) {
